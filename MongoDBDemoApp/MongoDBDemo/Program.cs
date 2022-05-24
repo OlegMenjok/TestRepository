e42e9636231f280b0596a90b2701c9ec38807374
OlegMenjok <89972613+OlegMenjok@users.noreply.github.com>
2022-05-24 10:20:04 +0300

    Add files via upload

---
+++ MongoDBDemoApp/MongoDBDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using MongoDBDemo;
+
+string connectionString = "mongodb://localhost:27017";
+string databaseName = "my_base_date";
+string collectionName = "My_date";
+
+var client = new MongoClient(connectionString);//зєднання з монгодб
+var db = client.GetDatabase(databaseName);//з якою бд ми хочемо спілкуватися
+var collection = db.GetCollection<PersonModel>(collectionName);//з якою колекцією ми хочемо спілкуватися
+
+var person = new PersonModel { FirstName = "Oleh", LastName = "Menjok" };
+
+await collection.InsertOneAsync(person);
+var results = await collection.FindAsync(_ => true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine(value:$"{result.Id}: {result.FirstName} {result.LastName}"); ;
+}
+
+
